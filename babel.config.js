2cfa24db44998ea00b2cdd2a837afac199ad5437
ytalo azeredo <ytaloazeredo@gmail.com>
2022-05-10 23:05:25 -0300

    Configuring babel

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@models': './src/models',
+        '@controllers': './src/controllers',
+        '@views': './src/views',
+        '@config': './src/config'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
